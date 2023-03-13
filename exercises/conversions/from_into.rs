a58a77d193c9d77fabad8a7a1bd1107407c7f9ee
caomao <joker_lizhih@163.com>
2023-03-13 15:46:12 +0800

    conversion solutions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,53 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let (name, age) = match s.split_once(',') {
+            Some((name, age)) => (name.trim(), age.trim()),
+            _ => return Person::default(),
+        };
+
+        if let Ok(age) = age.parse::<usize>() {
+            if name.len() > 0 {
+                return Person {
+                    name: name.into(),
+                    age,
+                }
+            }
+        };
+
+        Person::default()
+
+        // let parts: Vec<&str> = s.split(',').collect();
+        // if parts.len() == 2 && parts[0].len() > 0 {
+        //     let age: Result<usize, _> = parts[1].parse();
+        //     if let Ok(age) = age {
+        //         return Person {
+        //             name: parts[0].into(),
+        //             age: age.into(),
+        //         };
+        //     }
+        // }
+        // Person::default()
+
+
+        // if s.len() == 0 {
+        //     return Person::default();
+        // }
+        // let v: Vec<&str> = s.split(',').collect();
+        // if v.len() != 2 || v[0].len() == 0 {
+        //     return Person::default();
+        // }
+        // let age: usize = match v[1].parse::<usize>() {
+        //     Ok(age) => age,
+        //     Err(_) => return Person::default(),
+        // };
+        // Person {
+        //     name: v[0].to_string(),
+        //     age: age,
+        // }
     }
 }
 
